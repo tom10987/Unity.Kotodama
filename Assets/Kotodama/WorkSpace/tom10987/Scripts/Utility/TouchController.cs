f762055ea27db2c5db328f4ff856708f70435ac0
tom10987 <tom10987@gmail.com>
2015-10-20 22:09:29 +0900

    カメラを自動アスペクト比調整に対応

---
+++ Assets/Kotodama/WorkSpace/tom10987/Scripts/Utility/TouchController.cs
@@ -0,0 +1,61 @@
+﻿
+using UnityEngine;
+using Platform = UnityEngine.RuntimePlatform;
+
+
+public class TouchController : MonoBehaviour {
+
+  static int _touchId = -1;
+
+
+  static bool IsAndroid {
+    get { return Application.platform == Platform.Android; }
+  }
+
+  static bool IsIPhone {
+    get { return Application.platform == Platform.IPhonePlayer; }
+  }
+
+  static public bool IsSmartDevice {
+    get { return IsAndroid || IsIPhone; }
+  }
+
+
+  /// <summary>
+  /// タッチされた瞬間 true を返す
+  /// </summary>
+  static public bool IsTouchBegan() {
+    var count_ = Input.touchCount;
+    if (count_ <= 0) { return false; }
+
+    if (Input.touches[0].phase == TouchPhase.Began) {
+      _touchId = Input.touches[0].fingerId;
+      return true;
+    }
+
+    return false;
+  }
+
+  /// <summary>
+  /// タッチされ続けている間 true を返す
+  /// </summary>
+  static public bool IsTouchMoved() {
+    var count_ = Input.touchCount;
+    if (count_ <= 0) { return false; }
+
+    for (int i = 0; i < count_; ++i) {
+      if (_touchId == Input.touches[i].fingerId) { return true; }
+    }
+
+    return false;
+  }
+
+  /// <summary>
+  /// タッチが離された瞬間 true を返す
+  /// </summary>
+  /// <returns></returns>
+  static public bool IsTouchEnded() {
+    if (Input.touchCount <= 0) { return false; }
+    return Input.touches[0].phase == TouchPhase.Ended;
+  }
+}
