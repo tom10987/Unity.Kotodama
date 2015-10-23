c0d85fc40dfcf33f4919dfa7a23ffd2dd57158cb
tom10987 <tom10987@gmail.com>
2015-10-24 02:05:55 +0900

    デバッグ用にタッチとマウスの判定を自動判別するようにした

--- Assets/Kotodama/WorkSpace/tom10987/Scripts/Utility/ScreenInfo.cs
+++ Assets/Kotodama/WorkSpace/tom10987/Scripts/Utility/ScreenInfo.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ScreenInfo : MonoBehaviour {
 
-  // 画面サイズを返す
-  // TIPS: GameObject の Transform が Vector3 を使うため、Vector3 で返す
+  /// <summary>
+  /// 画面サイズを返す
+  /// </summary>
   static public Vector3 size {
+    // TIPS: GameObject の Transform が Vector3 を使うため、Vector3 で返す
     get {
       var width = (float)Screen.width;
       var height = (float)Screen.height;
@@ -14,12 +16,15 @@ public class ScreenInfo : MonoBehaviour {
     }
   }
 
-  // 画面中央の座標を返す
-  // TIPS: GameObject の Transform が Vector3 を使うため、Vector3 で返す
+  /// <summary>
+  /// 画面中央の座標を返す
+  /// </summary>
   static public Vector3 center { get { return size * 0.5f; } }
 
-  // 正投影カメラの Aspect、OrthoGraphicSize を反映させた画面比率を返す
-  static public Vector3 aspect {
+  /// <summary>
+  /// 正投影カメラの Aspect、OrthoGraphicSize を反映させた画面比率を返す
+  /// </summary>
+  static public Vector3 orthoAspect {
     get {
       var aspect = Camera.main.aspect;
       var orthoSize = Camera.main.orthographicSize;
