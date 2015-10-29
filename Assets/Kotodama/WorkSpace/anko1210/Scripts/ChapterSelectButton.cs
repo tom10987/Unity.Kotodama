83a70177ed5bbc454d378754c00593b9a47954fb
anko1210 <anko.anko.kyoko@gmail.com>
2015-10-29 15:41:06 +0900

    title update

---
+++ Assets/Kotodama/WorkSpace/anko1210/Scripts/ChapterSelectButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChapterSelectButton : MonoBehaviour {
+
+    public Text ChapMsg;
+    [SerializeField]
+    private string _chapter01Name = "";
+
+    SceneSequencer _sequencer = null;
+
+    void Start() {
+        _sequencer = FindObjectOfType<SceneSequencer>();
+    }
+
+    public void ChangeChapter1() {
+        //Application.LoadLevel(_chapter01Name);
+        _sequencer.SceneFinish("Chapter1");
+    }
+
+    public void ChangeChapter2()
+    {
+        ChapMsg.text = "チャプター２は解放されていません。";
+//        Application.LoadLevel("Chapter2");
+    }
+
+    public void ChangeChapter3()
+    {
+        ChapMsg.text = "チャプター３は解放されていません。";
+        //       Application.LoadLevel("Chapter3");
+    }
+
+}
---
+++ Assets/Kotodama/WorkSpace/anko1210/Scripts/TitleScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+    public GameObject StartCanvas;
+    public GameObject SelectCanvas;
+    public bool _changeCan = true;
+
+    public void Start()
+    {
+        StartCanvas.SetActive(_changeCan);
+        SelectCanvas.SetActive(!_changeCan);
+    }
+
+    public void ChangeCanvas()
+    {
+        if (StartCanvas.activeInHierarchy) { _changeCan = !_changeCan; }
+        else { _changeCan=!_changeCan; }
+
+        StartCanvas.SetActive(_changeCan);
+        SelectCanvas.SetActive(!_changeCan);
+    }
+
+
+
+}
