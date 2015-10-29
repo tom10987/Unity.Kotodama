d43a120020dfac4fea5a5c4d7dd1b58af770fba3
tom10987 <tom10987@gmail.com>
2015-10-29 14:30:36 +0900

    仮統合

--- Assets/Kotodama/WorkSpace/tom10987/Scripts/PlayerMove.cs
+++ Assets/Kotodama/WorkSpace/tom10987/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 ﻿
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 
 [Serializable]
@@ -11,6 +12,8 @@ public class SlantDirection {
   [Tooltip("斜め移動の角度（単位：度数法）")]
   float _angle = 30f;
 
+  // 度数法の値をラジアン値に変換した上で、
+  // 方向の基準となる単位ベクトルを返す
   Vector2 GetDirection() {
     var x = Mathf.Cos(_angle * Mathf.Deg2Rad);
     var y = Mathf.Sin(_angle * Mathf.Deg2Rad);
@@ -43,6 +46,40 @@ public class SlantDirection {
 }
 
 
+public class PlayerVector {
+
+  public enum Direction {
+    TopLeft,
+    Top,
+    TopRight,
+    Right,
+    BottomRight,
+    Bottom,
+    BottomLeft,
+    Left,
+  }
+
+  static Dictionary<Direction, Vector3> _direction = null;
+  static public Dictionary<Direction, Vector3> direction { get { return _direction; } }
+
+  static public void Init() {
+    _direction = new Dictionary<Direction, Vector3>();
+
+    _direction.Add(Direction.TopLeft, new Vector3(-1f, 1f, 0f).normalized);
+    _direction.Add(Direction.Top, Vector3.up);
+    _direction.Add(Direction.TopRight, new Vector3(1f, 1f, 0f).normalized);
+    _direction.Add(Direction.Right, Vector3.right);
+    _direction.Add(Direction.BottomRight, new Vector3(1f, -1f, 0f).normalized);
+    _direction.Add(Direction.Bottom, Vector3.down);
+    _direction.Add(Direction.BottomLeft, new Vector3(-1f, -1f, 0f).normalized);
+    _direction.Add(Direction.Left, Vector3.left);
+  }
+
+  static public void GetDirection() {
+  }
+}
+
+
 public class PlayerMove : MonoBehaviour {
 
   [SerializeField]
@@ -51,32 +88,33 @@ public class PlayerMove : MonoBehaviour {
 
   [SerializeField]
   [Tooltip("斜め移動の方向ベクトルを設定")]
-  SlantDirection _direction = null;
+  SlantDirection _slant = null;
 
+  Vector2 _direction = Vector2.zero;
   Rigidbody2D _ownRigid = null;
+  CircleCollider2D _collider = null;
 
 
   void Start() {
     _ownRigid = GetComponent<Rigidbody2D>();
-
-    Debug.Log(_direction.topLeft);
-    Debug.Log(_direction.topRight);
-    Debug.Log(_direction.bottomLeft);
-    Debug.Log(_direction.bottomRight);
+    _collider = GetComponent<CircleCollider2D>();
   }
 
   void Update() {
-    if (TouchController.IsTouchBegan()) { Translate(); }
+    if (TouchController.IsTouchBegan()) { Translate_(); }
   }
 
   void Translate() {
-    _ownRigid.velocity = Vector2.zero;
-
+    var touchPos = TouchController.GetTouchWorldPositionFromCamera();
+  }
+    
+  void Translate_() {
     var touchPos = TouchController.GetTouchWorldPositionFromCamera();
     var distance = touchPos - transform.position;
-    if (distance.magnitude < 1f) { return; }
 
-    var direction = distance.normalized * _velocity;
-    _ownRigid.AddForce(direction, ForceMode2D.Impulse);
+    var isStop = (distance.magnitude < _collider.radius);
+    var speed = distance.normalized * _velocity;
+    _direction = isStop ? Vector3.zero : speed;
+    _ownRigid.velocity = _direction;
   }
 }
