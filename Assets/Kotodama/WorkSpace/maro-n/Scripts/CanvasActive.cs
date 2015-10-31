a9aa17fdbbed7241b7a78cccefc010b0e3591b7b
U-PS14B06\vantan <with.tennis.win.0406@gmail.com>
2015-10-31 23:34:36 +0900

    メニュー画面のボタン、アドベンチャーパート（未完成）

---
+++ Assets/Kotodama/WorkSpace/maro-n/Scripts/CanvasActive.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class CanvasActive : MonoBehaviour {
+
+    public GameObject _adventureCanvas;
+    public bool _activeCanvas = false;
+	
+	void Update () {
+                
+        if (EventStart())
+        {
+            _activeCanvas = true;
+        }
+
+        if (EventEnd())
+        {
+            _activeCanvas = false;
+        }
+
+        _adventureCanvas.GetComponent<Canvas>().enabled = _activeCanvas;
+	}
+
+    public bool EventStart()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool EventEnd()
+    {
+        var text = GameObject.FindObjectOfType<TextBox>();
+       
+        if (text._current_text > text.MAX_TEXT)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
+
+
---
+++ Assets/Kotodama/WorkSpace/maro-n/Scripts/TextBox.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TextBox : MonoBehaviour {
+
+    public int MAX_TEXT = 4;
+
+    const string RESOURCE_PATH = "/Kotodama/WorkSpace/maro-n/Resource/JSON/";
+
+    [SerializeField]
+    string JSON_PATH = "";
+
+    private string[] _text;
+    public int _current_text;
+    public Text _textBox;
+
+    void Awake()
+    {
+        _text = new string[MAX_TEXT];
+    }
+
+    void Start()
+    {
+        var text = File.ReadAllText(Application.dataPath + RESOURCE_PATH + JSON_PATH);
+
+        JsonNode json = JsonNode.Parse(text);
+
+        _current_text = 0;
+
+        for (int i = 0; i < MAX_TEXT; ++i) {
+            var temp_text = json["Texts"][i].Get<string>();
+
+            _text[i] = temp_text;
+            Debug.Log(_text[i]);
+        }
+
+        _textBox.text = _text[_current_text];
+    }
+
+    void TextUpdate()
+    {
+        var activeCanvas = GameObject.FindObjectOfType<CanvasActive>();
+
+        if (activeCanvas.EventStart())
+        {
+            _textBox.text = _text[_current_text];
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                _current_text++;
+            }
+        }
+    }
+
+    void Update()
+    {
+        TextUpdate();
+    }
+}
