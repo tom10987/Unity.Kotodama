a32f1b0a4a0f3a360999206bd31c6353c899b518
tom10987 <tom10987@gmail.com>
2015-12-02 21:24:37 +0900

    タッチ座標取り出し修正、他

--- Assets/Kotodama/Scripts/tom10987/Audio/AudioManager.cs
+++ Assets/Kotodama/Scripts/tom10987/Audio/AudioManager.cs
@@ -3,4 +3,8 @@ using UnityEngine;
 
 
 public class AudioManager : SingletonBehaviour<AudioManager> {
+
+  protected override void Awake() {
+    base.Awake();
+  }
 }
--- Assets/Kotodama/Scripts/tom10987/Utility/ObjectTagList.cs
+++ Assets/Kotodama/Scripts/tom10987/Utility/ObjectTagList.cs
@@ -1,11 +1,35 @@
 ﻿
+using System.Collections.Generic;
+
+
 public class ObjectTag {
 
-  static readonly string[] _name = {
-    "Player",
-    "Enemy",
-    "UI",
+  static public string player { get { return "Player"; } }
+  static public string enemy { get { return "Enemy"; } }
+  static public string ui { get { return "UI"; } }
+
+  static List<string> _name = new List<string> {
+    player,
+    enemy,
+    ui,
   };
 
-  static public string[] name { get { return _name; } }
+  static public List<string> name { get { return _name; } }
+}
+
+public class SceneTag {
+
+  static public string title { get { return "Title"; } }
+  static public string prologue { get { return "Prologue"; } }
+  static public string mainGame { get { return "MainGame"; } }
+  static public string epilogue { get { return "Epilogue"; } }
+
+  static List<string> _name = new List<string> {
+    title,
+    prologue,
+    mainGame,
+    epilogue,
+  };
+
+  static public List<string> name { get { return _name; } }
 }
