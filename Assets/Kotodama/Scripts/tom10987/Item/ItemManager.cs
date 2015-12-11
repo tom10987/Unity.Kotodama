5fd75914b303aa61897d3414ec7f83a5f7f7ca0c
tom10987 <tom10987@gmail.com>
2015-12-11 18:14:17 +0900

    メニュー画面仕様変更、完了

--- Assets/Kotodama/Scripts/tom10987/Item/ItemManager.cs
+++ Assets/Kotodama/Scripts/tom10987/Item/ItemManager.cs
@@ -7,6 +7,8 @@ public class ItemManager : SingletonBehaviour<ItemManager> {
   Dictionary<ItemName, ItemState> _items = null;
   public Dictionary<ItemName, ItemState> items { get { return _items; } }
 
+  public void UpdateItemState(ItemName name, bool state) { items[name].UpdateState(state); }
+
 
   protected override void Awake() {
     base.Awake();
