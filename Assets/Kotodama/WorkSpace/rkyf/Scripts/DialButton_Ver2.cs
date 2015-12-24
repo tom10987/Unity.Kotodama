c79596d201f52e99c49eea1683409e494eb9b8c6
rkyf <finight.lnk@gmail.com>
2015-12-24 17:35:48 +0900

    掲示板など

---
+++ Assets/Kotodama/WorkSpace/rkyf/Scripts/DialButton_Ver2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DialButton_Ver2 : MonoBehaviour
+{
+    [SerializeField]
+    Text Dial;
+
+    [SerializeField]
+    string TrueNumber = null;
+
+    [SerializeField]
+    public bool clear = false;
+
+    [SerializeField]
+    int StringLimit = 10;
+
+    void Start()
+    {
+        Dial.text = null;
+    }
+	
+    public void Number(int i)
+    {
+        if(Dial.text.Length < StringLimit)
+        Dial.text += i.ToString();
+    }
+
+    public void BackSpace()
+    {
+        Dial.text = null;
+    }
+
+    public void Enter()
+    {
+        if(Dial.text == TrueNumber)
+        {
+            clear = true;
+        }
+    }
+}
