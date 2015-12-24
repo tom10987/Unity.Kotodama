80e40666ed000511d591e81b2ea566ef9571dbf8
tom10987 <tom10987@gmail.com>
2015-12-24 18:11:01 +0900

    音楽素材導入、仮統合

--- Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
+++ Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameManager : SingletonBehaviour<GameManager> {
 
-  public bool isPause { get; set; }
-
   protected override void Awake() {
-    //TODO: シーンが変わってもオブジェクトが残るようにする
+    base.Awake();
     DontDestroyOnLoad(gameObject);
+  }
+
+  void Start() {
   }
 }
