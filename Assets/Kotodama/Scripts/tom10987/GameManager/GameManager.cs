67705c186be28c1836955b6e6b5e2db9787edf71
tom10987 <tom10987@gmail.com>
2015-12-25 15:12:11 +0900

    シーン切り替えのバグ修正

--- Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
+++ Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class GameManager : SingletonBehaviour<GameManager> {
 
+  [SerializeField]
+  GameObject _sceneSequencer = null;
+
+  [SerializeField]
+  GameObject _audioManager = null;
+
   public bool isPause { get; set; }
+
 
   protected override void Awake() {
     base.Awake();
