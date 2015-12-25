8ab7baf51fea8abc81eff1b06d25642d6c0482f5
tom10987 <tom10987@gmail.com>
2015-12-25 13:37:37 +0900

    神社エリアのプレハブ更新

--- Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
+++ Assets/Kotodama/Scripts/tom10987/GameManager/GameManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class GameManager : SingletonBehaviour<GameManager> {
 
+  public bool isPause { get; set; }
+
   protected override void Awake() {
     base.Awake();
     DontDestroyOnLoad(gameObject);
   }
 
   void Start() {
+    isPause = false;
   }
 }
