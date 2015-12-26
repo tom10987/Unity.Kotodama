7f9a71464fbaa44e55c00cc33d63849369a5a396
kobayasi <wing.road720@outlook.com>
2015-12-26 10:01:40 +0900

    pull

---
+++ Assets/Kotodama/WorkSpace/kobayasi720/Script/Manhole.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Manhole : MonoBehaviour {
+
+    [SerializeField]
+    GameObject manhole;
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Application.isEditor)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                    {
+                        if (hit.transform.gameObject.tag != "Player")
+                        {
+                            //var obj = GameObject.Instantiate(Pre) as GameObject;
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+}
