090817544b605ecf438a13c6234c537136c5e3c8
U-PS14B06\vantan <with.tennis.win.0406@gmail.com>
2016-01-08 10:41:10 +0900

    迷路ステージ（未完）

---
+++ Assets/Kotodama/Scripts/maro-n/Gimmick/TouchGimmick.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TouchGimmick : MonoBehaviour {
+
+    public bool _isHit = false;
+    public bool _isClear = false;
+
+    void OnCollisionStay(Collision player)
+    {
+        // 灯籠の床部にプレイヤーが接触してる間
+        if (player.gameObject.tag.Equals(ObjectTag.player))
+        {
+            _isHit = true;
+        }
+        else
+        {
+            _isHit = false;
+        }
+    }
+
+    void ClearGimmick()
+    {
+        if (_isHit)
+        {
+            // 画面をタッチした時
+            if (TouchController.IsTouchBegan())
+            {
+                // カメラからタッチ位置へのレイを生成
+                Ray ray_ = Camera.main.ScreenPointToRay(TouchController.GetTouchScreenPosition());
+                RaycastHit hit_ = new RaycastHit();
+
+                // レイが当たったオブジェクトのコライダーを取得
+                if (Physics.Raycast(ray_, out hit_))
+                {
+                    if (hit_.collider.gameObject.tag.Equals(ObjectTag.gimmick))
+                    {
+                        // クリアフラグをtrueにする
+                        if (!_isClear)
+                        {
+                            _isClear = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        ClearGimmick();
+    }
+}
