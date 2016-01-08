42f9ff7d5ffc2bc9e702b4e8c8fd2583a93ef07e
rkyf <finight.lnk@gmail.com>
2016-01-08 16:17:13 +0900

    0108更新

---
+++ Assets/Kotodama/WorkSpace/rkyf/Scripts/EventChecker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventChecker : MonoBehaviour
+{
+    [SerializeField]
+    bool Alive = false;
+
+    void LateUpdate ()
+    {
+        EventContinue();
+	}
+
+    public void EventContinue()
+    {
+        if(!DialButton_Ver2.ClearFlug_PublicPhone) return;
+        Stop();
+    }
+
+    public bool SetAlive()
+    {
+        return Alive;
+        
+    }
+
+    public void Stop()
+    {
+        this.gameObject.SetActive(SetAlive());
+    }
+
+}
