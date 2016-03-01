c74b01f2c34494dda291dc9b91d679ea668efccb
tom10987 <tom10987@gmail.com>
2016-03-01 21:57:17 +0900

    フォルダリネーム、プレイヤー挙動修正

--- Assets/Kotodama/Scripts/tom10987/Player/PlayerMove.cs
+++ Assets/Kotodama/Scripts/tom10987/Player/PlayerMove.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-  public void OnTouch() { PlayerState.instance.Move(); }
+  [SerializeField]
+  LayerMask _mask;
+
+  [SerializeField]
+  NavMeshAgent _agent = null;
+
+  void Update() {
+    if (!TouchController.IsTouchBegan()) { return; }
+
+    var touch = TouchController.GetTouchScreenPosition();
+    var ray = Camera.main.ScreenPointToRay(touch);
+    RaycastHit hit;
+    if (!Physics.Raycast(ray, out hit, 100f, _mask.value)) { return; }
+    if (hit.transform == null) { return; }
+
+    _agent.SetDestination(hit.point);
+  }
 }
