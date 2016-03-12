763ea5516ad645d462e51cad614d7987398be8fe
tom10987 <tom10987@gmail.com>
2016-03-12 21:24:34 +0900

    掲示板完了

--- Unity.Kotodama/Assets/Scripts/Gimmick/Board/BoardTrigger.cs
+++ Unity.Kotodama/Assets/Scripts/Gimmick/Board/BoardTrigger.cs
@@ -1,13 +1,10 @@
 ﻿
 using UnityEngine;
 
-
 public class BoardTrigger : MonoBehaviour {
 
-  EnemyManager enemy { get { return EnemyManager.instance; } }
-
   [SerializeField]
-  GameObject _memoPrefab = null;
+  BoardMemo _memoPrefab = null;
 
   [SerializeField]
   [Tooltip("メモの色を指定")]
@@ -17,24 +14,17 @@ public class BoardTrigger : MonoBehaviour {
   [Tooltip("テキストの色を指定")]
   Color _textColor = Color.white;
 
-  // FIXME:
-  // インスペクター上の編集がしにくいので、自動的にボックスを拡大する方法を調べる
   [SerializeField]
-  [Multiline(7)]
+  [Multiline(10)]
   string _text = string.Empty;
 
-
   void OnTriggerEnter(Collider other) {
-    if (other.tag != ObjectTag.Player.ToString()) { return; }
+    if (!ObjectTag.Player.EqualTo(other.tag)) { return; }
 
     // TIPS: メモを生成
-    var instance = Instantiate(_memoPrefab).GetComponent<BoardMemo>();
+    var instance = Instantiate(_memoPrefab);
     instance.memo.color = _textColor;
     instance.memo.text = _text;
     instance.memoImage.color = _memoColor;
-
-    // TIPS: プレイヤーと敵キャラを停止する
-    PlayerState.instance.Stop();
-    GameManager.instance.Pause();
   }
 }
