6bf842e26345c228514fd6155385d1275555af3e
tom10987 <tom10987@gmail.com>
2016-03-12 19:21:54 +0900

    ウィンドウ表示の仕様変更

--- Unity.Kotodama/Assets/Scripts/Item/ItemCollider.cs
+++ Unity.Kotodama/Assets/Scripts/Item/ItemCollider.cs
@@ -7,6 +7,10 @@ public class ItemCollider : MonoBehaviour {
   [Tooltip("触れると取得できる、アイテムのプレハブを指定")]
   ItemIcon _item = null;
 
+  [SerializeField]
+  [Range(0.5f, 2.0f)]
+  float _messageTime = 1.0f;
+
   void OnTriggerEnter(Collider other) {
     if (!ObjectTag.Player.EqualTo(other.tag)) { return; }
 
@@ -14,7 +18,7 @@ public class ItemCollider : MonoBehaviour {
     var success = ItemManager.instance.Add(_item);
     if (!success) { ItemManager.instance.GetItem(_item.type).useItem = false; }
     Destroy(gameObject);
-    WindowManager.instance.CreateMessageWindow(GetItemMessage(), 0.5f);
+    WindowManager.instance.CreateMessageWindow(GetItemMessage(), _messageTime);
   }
 
   string GetItemMessage() { return _item.type.GetName() + "を拾った"; }
